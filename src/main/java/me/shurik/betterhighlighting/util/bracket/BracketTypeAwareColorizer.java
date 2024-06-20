15e8cd1480e80b735f84283f9e866ed1d863618a
shurik204 <iwmdiif@gmail.com>
2024-06-20 23:29:43 +0300

    Implement bracket-independent colorizer

--- src/main/java/me/shurik/betterhighlighting/util/bracket/BracketTypeAwareColorizer.java
+++ src/main/java/me/shurik/betterhighlighting/util/bracket/BracketTypeAwareColorizer.java
@@ -3,12 +3,14 @@ package me.shurik.betterhighlighting.util.bracket;
 import me.shurik.betterhighlighting.api.syntax.BracketColorizer;
 import net.minecraft.network.chat.Style;
 
+import java.util.Stack;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * A {@link BracketColorizer} that tracks color indices for each bracket type separately.
  */
 public class BracketTypeAwareColorizer extends BaseBracketColorizer {
+    private final Stack<BracketType> brackets = new Stack<>();
     private final AtomicInteger[] indices = new AtomicInteger[] {new AtomicInteger(0), new AtomicInteger(0), new AtomicInteger(0)};
 
     public BracketTypeAwareColorizer(BracketStyles styles) {
@@ -17,7 +19,24 @@ public class BracketTypeAwareColorizer extends BaseBracketColorizer {
 
     @Override
     public Style getBracketStyle(BracketType bracketType) {
-         return styles.getStyle(modifyIndex(bracketType, indices[bracketType.index]));
+        if (bracketType.closing) {
+            // If we get a closing bracket and there are no opening brackets, we have an unmatched bracket
+            if (brackets.isEmpty()) {
+                return styles.unmatchedBracketStyle();
+            }
+            // If the closing bracket matches the last opening bracket, pop it from the stack and return the matching style
+            if (bracketType.matchesClosing(brackets.peek())) {
+                int index = modifyIndex(bracketType, indices[bracketType.index]);
+                brackets.pop();
+                return styles.getStyle(index);
+            } else {
+                // If the closing bracket does not match the last opening bracket, we have an unmatched bracket
+                return styles.unmatchedBracketStyle();
+            }
+        }
+        // Opening bracket, push it into the stack and return the style
+        brackets.push(bracketType);
+        return styles.getStyle(modifyIndex(bracketType, indices[bracketType.index]));
     }
 
     private int modifyIndex(BracketType bracket, AtomicInteger index) {
